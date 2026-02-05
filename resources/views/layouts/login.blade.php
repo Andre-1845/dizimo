1e233d929bae6a0dd21616a65d3b8e4bd04d7e15
Andre Andrino <andrebaumgratz@gmail.com>
2026-02-04 21:22:48 -0300

    fix login layout vite entry

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app_auth.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-login">
